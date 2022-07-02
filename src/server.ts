b16a6627f89551b3bac22154bbec7196cc1f93d6
Ahmed Saeed <87494410+ahmedsaeed195@users.noreply.github.com>
2022-07-02 17:27:37 +0200

    set postgres and .env files switching

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
 
 app.get('/', (req: express.Request, res: express.Response) => {
     return res.send('Hello World');
 });
 
-const PORT = process.env.PORT || 3000;
+console.log(process.env.NODE_ENV)
 
 app.listen(PORT, (): void => console.log(`Listening on port ${PORT}`));
