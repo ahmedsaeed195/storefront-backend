2d186cd5289547037cec6f287eab9215ff548d01
Ahmed Saeed <87494410+ahmedsaeed195@users.noreply.github.com>
2022-07-04 04:41:33 +0200

    updated dotenv settings

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
