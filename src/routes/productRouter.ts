acf09f0f3b12d8f97faac985e1ea9b3d71db7279
Ahmed Saeed <87494410+ahmedsaeed195@users.noreply.github.com>
2022-07-08 17:29:58 +0200

    better controller naming convension

--- src/routes/productRouter.ts
+++ src/routes/productRouter.ts
@@ -1,24 +1,24 @@
 import { Router } from "express";
-import ProductsController from '../controller/productsController'
+import ProductController from '../controller/productController'
 import ProductValidator from '../middleware/validation/product/productValidator'
 import productUpdateValidator from '../middleware/validation/product/productUpdateValidator'
 
-const productsController = new ProductsController()
+const productController = new ProductController()
 const productRouter = Router()
 
 //* GET /product
-productRouter.get('/', productsController.index)
+productRouter.get('/', productController.index)
 
 //* GET /product/:id
-productRouter.get('/:id', productsController.show)
+productRouter.get('/:id', productController.show)
 
 //* POST /product
-productRouter.post('/', ProductValidator, productsController.store)
+productRouter.post('/', ProductValidator, productController.store)
 
 //* PUT /product/:id
-productRouter.put('/:id', productUpdateValidator, productsController.update)
+productRouter.put('/:id', productUpdateValidator, productController.update)
 
 //* DELETE /product/:id
-productRouter.delete('/:id', productsController.delete)
+productRouter.delete('/:id', productController.delete)
 
 export default productRouter
