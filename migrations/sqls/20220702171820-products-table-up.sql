5ccb0cbfec58b6ff89da406dc159760f49ab85f9
Ahmed Saeed <87494410+ahmedsaeed195@users.noreply.github.com>
2022-07-08 17:37:17 +0200

    added not null constraint to some columns

--- migrations/sqls/20220702171820-products-table-up.sql
+++ migrations/sqls/20220702171820-products-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    price FLOAT,
+    name VARCHAR(100) NOT NULL,
+    price FLOAT NOT NULL,
     category VARCHAR(50)
 );
--- migrations/sqls/20220707134534-users-table-up.sql
+++ migrations/sqls/20220707134534-users-table-up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50),
+    username VARCHAR(50) NOT NULL,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    password_digest VARCHAR(60)
+    password_digest VARCHAR(60) NOT NULL
 );
 CREATE UNIQUE INDEX username ON users(username);
