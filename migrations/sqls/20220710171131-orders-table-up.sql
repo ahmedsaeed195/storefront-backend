81189eb589d8e476fbf499fec405f6c1c1bf7c9c
Ahmed Saeed <87494410+ahmedsaeed195@users.noreply.github.com>
2022-07-12 20:13:44 +0200

    added on parent delete constaint

--- migrations/sqls/20220710171131-orders-table-up.sql
+++ migrations/sqls/20220710171131-orders-table-up.sql
@@ -2,6 +2,6 @@ CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
     status BOOLEAN NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 CREATE UNIQUE INDEX user_id ON orders(user_id);
