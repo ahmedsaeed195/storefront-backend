601a0bc27e21c609eb6d73f18e039e4c902d9b01
Ahmed Saeed <87494410+ahmedsaeed195@users.noreply.github.com>
2022-07-14 21:50:38 +0200

    installed cors package to enable cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes'
+import cors from 'cors'
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
